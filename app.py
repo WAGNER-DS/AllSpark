11892fdd4512bc7056409e5f42694f9202e41ded
WAGNER-DS <67823891+WAGNER-DS@users.noreply.github.com>
2025-04-23 11:57:40 -0300

    Update app.py

--- app.py
+++ app.py
@@ -108,5 +108,5 @@ def roteador(pathname, session_data):
 
 # Executa
 if __name__ == "__main__":
-    app.run(debug=True)
-    #app.run(debug=True, host="0.0.0.0", port=8050)
+    #app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0", port=8050)
